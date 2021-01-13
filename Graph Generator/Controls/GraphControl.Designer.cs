bca308cd73040e18b32a1c977846b6c9bf9dc1fd
Arne (Wahrstorf) <aleitert@kent.edu>
2021-01-13 18:17:38 +0100

    Copied existing logic and functionality (from MainForm) into GraphControl.

--- Graph Generator/Controls/GraphControl.Designer.cs
+++ Graph Generator/Controls/GraphControl.Designer.cs
@@ -29,10 +29,40 @@ namespace GraphGenerator
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.components = new System.ComponentModel.Container();
+            this.pnlCanves = new System.Windows.Forms.Panel();
+            this.drawingTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pnlCanves
+            // 
+            this.pnlCanves.BackColor = System.Drawing.Color.White;
+            this.pnlCanves.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCanves.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlCanves.Location = new System.Drawing.Point(0, 0);
+            this.pnlCanves.Name = "pnlCanves";
+            this.pnlCanves.Size = new System.Drawing.Size(150, 150);
+            this.pnlCanves.TabIndex = 3;
+            this.pnlCanves.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlCanves_Paint);
+            // 
+            // drawingTimer
+            // 
+            this.drawingTimer.Tick += new System.EventHandler(this.drawingTimer_Tick);
+            // 
+            // GraphControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlCanves);
+            this.Name = "GraphControl";
+            this.Resize += new System.EventHandler(this.GraphControl_Resize);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlCanves;
+        private System.Windows.Forms.Timer drawingTimer;
     }
 }
--- Graph Generator/Controls/GraphControl.cs
+++ Graph Generator/Controls/GraphControl.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace GraphGenerator
@@ -8,6 +10,184 @@ namespace GraphGenerator
         public GraphControl()
         {
             InitializeComponent();
+            typeof(Control).GetProperty("DoubleBuffered", (System.Reflection.BindingFlags)(-1)).SetValue(pnlCanves, true, new object[] { });
+        }
+
+        private void GraphControl_Resize(object sender, EventArgs e)
+        {
+            CenterGraph();
+            Refresh();
+        }
+
+        private void pnlCanves_Paint(object sender, PaintEventArgs e)
+        {
+            PaintGraph(e.Graphics, pnlCanves.Width, pnlCanves.Height);
+        }
+
+        private void drawingTimer_Tick(object sender, EventArgs e)
+        {
+            DrawGraph();
+            Refresh();
+
+            if (Environment.TickCount - drawingTime > MaxDrawTime)
+            {
+                drawingTimer.Stop();
+            }
+        }
+
+
+        private Graph graph = null;
+        private Vector[] drawing = null;
+
+        private int drawingTime = -1;
+        private const int MaxDrawTime = 3000; // in ms
+
+
+        internal Graph Graph
+        {
+            get
+            {
+                return graph;
+            }
+            set
+            {
+                graph = value;
+                drawing = null;
+                StartDrawing();
+            }
+        }
+
+        /// <summary>
+        /// Starts the process of drawing the graph.
+        /// The graph will be redrawn for a few seconds.
+        /// </summary>
+        private void StartDrawing()
+        {
+            drawingTime = Environment.TickCount;
+            drawingTimer.Start();
+        }
+
+        /// <summary>
+        /// Computes the x- and y-coordinates for each vertex of the given graph using a forced-based algorithm.
+        /// </summary>
+        private void DrawGraph()
+        {
+            if (graph == null)
+            {
+                drawing = null;
+                return;
+            }
+
+            drawing = graph.Draw(drawing);
+            CenterGraph();
+        }
+
+        /// <summary>
+        /// Centers tha graph at origin of coordinate system, i.e., at (0, 0).
+        /// </summary>
+        private void CenterGraph()
+        {
+            if (drawing == null) return;
+
+
+            double minX = double.MaxValue;
+            double minY = double.MaxValue;
+
+            double maxX = double.MinValue;
+            double maxY = double.MinValue;
+
+            for (int vId = 0; vId < graph.Size; vId++)
+            {
+                Vector vecV = drawing[vId];
+
+                minX = Math.Min(minX, vecV.X);
+                minY = Math.Min(minY, vecV.Y);
+
+                maxX = Math.Max(maxX, vecV.X);
+                maxY = Math.Max(maxY, vecV.Y);
+            }
+
+            Vector shift = new Vector(minX + maxX, minY + maxY);
+            shift *= -0.5;
+
+            for (int vId = 0; vId < graph.Size; vId++)
+            {
+                drawing[vId] += shift;
+            }
+        }
+
+        /// <summary>
+        /// Uses the given graphics object to paint the graph centered in the are of the given size.
+        /// </summary>
+        private void PaintGraph(Graphics g, int width, int height)
+        {
+            /*
+             * ToDo: Potential improvements.
+             *   - Give a rectangle instaed of just a size.
+             *   - Undo configurations of g.
+             */
+
+            g.Clear(pnlCanves.BackColor);
+
+            if (graph == null || drawing == null)
+            {
+                return;
+            }
+
+
+            // --- Determine scale. ---
+
+            double minX = double.MaxValue;
+            double minY = double.MaxValue;
+
+            double maxX = double.MinValue;
+            double maxY = double.MinValue;
+
+            for (int vId = 0; vId < graph.Size; vId++)
+            {
+                Vector vecV = drawing[vId];
+
+                minX = Math.Min(minX, vecV.X);
+                minY = Math.Min(minY, vecV.Y);
+
+                maxX = Math.Max(maxX, vecV.X);
+                maxY = Math.Max(maxY, vecV.Y);
+            }
+
+            double scaleX = 0.8 * (double)width / (maxX - minX);
+            double scaleY = 0.8 * (double)height / (maxY - minY);
+
+            float scale = Convert.ToSingle(Math.Min(scaleX, scaleY));
+
+
+            // --- Draw graph. ---
+
+            g.TranslateTransform(width / 2, height / 2);
+            g.SmoothingMode = SmoothingMode.HighQuality;
+
+            const float PenWidth = 1F;
+            Pen blackPen = new Pen(Color.Black, PenWidth);
+
+            // Draw edges.
+            for (int vId = 0; vId < graph.Size; vId++)
+            {
+                foreach (int uId in graph[vId])
+                {
+                    if (uId < vId) continue;
+                    g.DrawLine
+                    (
+                        blackPen,
+                        (drawing[vId] * scale).ToPointF(),
+                        (drawing[uId] * scale).ToPointF()
+                    );
+                }
+            }
+
+            for (int vId = 0; vId < graph.Size; vId++)
+            {
+                PointF ptV = (drawing[vId] * scale).ToPointF();
+                g.FillEllipse(Brushes.DarkGreen, ptV.X - 5, ptV.Y - 5, 11, 11);
+            }
         }
     }
 }
