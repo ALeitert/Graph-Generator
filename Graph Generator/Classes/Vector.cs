7b0c16c4d93884f06b498db6f0fe0c75966bef08
Arne (Wahrstorf) <aleitert@kent.edu>
2021-01-13 14:36:33 +0100

    Added vector struct since PointF does not have operators defined.

---
+++ Graph Generator/Classes/Vector.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+
+namespace GraphGenerator
+{
+    public struct Vector
+    {
+        public Vector(PointF p)
+        {
+            this.X = p.X;
+            this.Y = p.Y;
+        }
+
+        public Vector(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public double Length
+        {
+            get { return Math.Sqrt(X * X + Y * Y); }
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector operator /(Vector v, double m)
+        {
+            return new Vector(v.X / m, v.Y / m);
+        }
+
+        public static Vector operator *(Vector v, double m)
+        {
+            return new Vector(v.X * m, v.Y * m);
+        }
+
+        public static Vector operator *(double m, Vector v)
+        {
+            return new Vector(v.X * m, v.Y * m);
+        }
+
+        public Vector Normalize()
+        {
+            if (Length == 0) return this;
+
+            return this / Length;
+        }
+
+        public PointF ToPointF()
+        {
+            return new PointF(Convert.ToSingle(X), Convert.ToSingle(Y));
+        }
+
+    }
+}
