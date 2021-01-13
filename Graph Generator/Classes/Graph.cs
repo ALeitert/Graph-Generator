6f72f80f66294a365eba8b5d4dc75618448afd00
Arne (Wahrstorf) <aleitert@kent.edu>
2021-01-13 13:23:11 +0100

    Added basic graph class.

---
+++ Graph Generator/Classes/Graph.cs
@@ -0,0 +1,29 @@
+﻿
+namespace GraphGenerator
+{
+    class Graph
+    {
+        int[][] adjList = null;
+
+        public Graph(int size)
+        {
+            adjList = new int[size][];
+        }
+
+        public int Size
+        {
+            get
+            {
+                return adjList.Length;
+            }
+        }
+
+        public int[] this[int vId]
+        {
+            get
+            {
+                return adjList[vId];
+            }
+        }
+    }
+}
