8248e9561ff8d43a4f001071bed24414913fc389
Arne (Wahrstorf) <aleitert@kent.edu>
2021-01-13 17:46:28 +0100

    Added empty control to draw graphs.

---
+++ Graph Generator/Controls/GraphControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GraphGenerator
+{
+    public partial class GraphControl : UserControl
+    {
+        public GraphControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
