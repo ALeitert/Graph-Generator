43380ac6785ef03683e0490deed4024c13487270
Arne (Wahrstorf) <aleitert@kent.edu>
2021-01-13 13:10:10 +0100

    Added basic interface.

--- Graph Generator/MainForm.Designer.cs
+++ Graph Generator/MainForm.Designer.cs
@@ -29,19 +29,84 @@ namespace GraphGenerator
         /// </summary>
         private void InitializeComponent()
         {
+            this.mainToolBar = new System.Windows.Forms.ToolStrip();
+            this.pnlCanves = new System.Windows.Forms.Panel();
+            this.pnlToolBar = new System.Windows.Forms.Panel();
+            this.btnGenerate = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mainToolBar.SuspendLayout();
+            this.pnlToolBar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // mainToolBar
+            // 
+            this.mainToolBar.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.mainToolBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.btnGenerate});
+            this.mainToolBar.Location = new System.Drawing.Point(0, 0);
+            this.mainToolBar.Name = "mainToolBar";
+            this.mainToolBar.Size = new System.Drawing.Size(776, 25);
+            this.mainToolBar.TabIndex = 0;
+            // 
+            // pnlCanves
+            // 
+            this.pnlCanves.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlCanves.BackColor = System.Drawing.Color.White;
+            this.pnlCanves.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCanves.Location = new System.Drawing.Point(12, 43);
+            this.pnlCanves.Name = "pnlCanves";
+            this.pnlCanves.Size = new System.Drawing.Size(776, 395);
+            this.pnlCanves.TabIndex = 2;
+            // 
+            // pnlToolBar
+            // 
+            this.pnlToolBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlToolBar.Controls.Add(this.mainToolBar);
+            this.pnlToolBar.Location = new System.Drawing.Point(12, 12);
+            this.pnlToolBar.Name = "pnlToolBar";
+            this.pnlToolBar.Size = new System.Drawing.Size(776, 25);
+            this.pnlToolBar.TabIndex = 3;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(58, 22);
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlToolBar);
+            this.Controls.Add(this.pnlCanves);
             this.Name = "MainForm";
             this.Text = "Graph Generator";
+            this.mainToolBar.ResumeLayout(false);
+            this.mainToolBar.PerformLayout();
+            this.pnlToolBar.ResumeLayout(false);
+            this.pnlToolBar.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip mainToolBar;
+        private System.Windows.Forms.ToolStripButton btnGenerate;
+        private System.Windows.Forms.Panel pnlCanves;
+        private System.Windows.Forms.Panel pnlToolBar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
     }
 }
--- Graph Generator/MainForm.cs
+++ Graph Generator/MainForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GraphGenerator
@@ -15,6 +8,12 @@ namespace GraphGenerator
         public MainForm()
         {
             InitializeComponent();
+            typeof(Control).GetProperty("DoubleBuffered", (System.Reflection.BindingFlags)(-1)).SetValue(pnlCanves, true, new object[] { });
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
