d884a847a96b6d07cc619c98c5796c9a52f462ae
Arne (Wahrstorf) <aleitert@kent.edu>
2021-01-14 14:51:39 +0100

    Added geometry class and moved generation of random points into it.

---
+++ Graph Generator/Classes/Geometry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphGenerator
+{
+    class Geometry
+    {
+        public static Vector[] GetRandomPoints(int size, double width, double height)
+        {
+            Random rng = new Random();
+            Vector[] points = new Vector[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                points[i] = new Vector
+                (
+                    rng.NextDouble() * width,
+                    rng.NextDouble() * height
+                );
+            }
+
+            return points;
+        }
+
+    }
+}
