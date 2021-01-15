58425768e9fe16ecbb545e66570a71a25c734e51
Arne (Wahrstorf) <aleitert@kent.edu>
2021-01-15 18:51:00 +0100

    Added computation of convex hull via Graham Scan algorithm.

--- Graph Generator/Classes/Geometry.cs
+++ Graph Generator/Classes/Geometry.cs
@@ -68,6 +68,7 @@ namespace GraphGenerator
 
         /// <summary>
         /// Returns an equilateral triangle that sorounds the given points.
+        /// Points are in counter-clock-wise order in a left-handed coordinate system.
         /// </summary>
         public static Vector[] GetSurroundingTriangle(Vector[] points)
         {
@@ -138,6 +139,143 @@ namespace GraphGenerator
             return
                 (scalXY <= 0 && scalYZ <= 0 && scalZX <= 0) ||
                 (scalXY >= 0 && scalYZ >= 0 && scalZX >= 0);
+        }
+
+        /// <summary>
+        /// Computes the convex hull from a set of points going counter-clockwise starting at the left-most point.
+        /// </summary>
+        public static int[] GetConvexHull(Vector[] points)
+        {
+            // Hull computed via Graham Scan.
+
+
+            // --- Find left-most point. ---
+
+            int lmId = 0;
+
+            for (int i = 1; i < points.Length; i++)
+            {
+                if (points[i].X < points[lmId].X)
+                {
+                    lmId = i;
+                }
+            }
+
+
+            // --- Sort points based on angle to left-most point. ---
+
+            int[] ids = new int[points.Length];
+            for (int i = 0; i < points.Length; i++)
+            {
+                ids[i] = i;
+            }
+
+            GrahamScanComp com = new GrahamScanComp(lmId, points);
+            Array.Sort(ids, com);
+
+
+            // --- Compute convex hull. ---
+
+            List<int> chStack = new List<int>();
+            chStack.Add(ids[0]);
+            chStack.Add(ids[1]);
+
+            for (int i = 2; i < ids.Length; i++)
+            {
+                int iId = ids[i];
+
+                // Pop points from stack until P_2 -- P_1 -- P_i forms a left turn.
+                for (Vector ptI = points[iId]; ; )
+                {
+                    int min1Id = chStack[chStack.Count - 1];
+                    int min2Id = chStack[chStack.Count - 2];
+
+                    Vector pt1 = points[min1Id];
+                    Vector pt2 = points[min2Id];
+
+                    if (GetScalar(pt2, pt1, ptI) <= 0) break;
+                    if (chStack.Count <= 2) break;
+
+                    chStack.RemoveAt(chStack.Count - 1);
+                }
+
+                chStack.Add(iId);
+            }
+
+            return chStack.ToArray();
+        }
+
+        /// <summary>
+        /// Allows to compare points based on their angle around a given point.
+        /// The smalles possible position for a point is at -180° (inclusive) and the largest at +180° (exclusive).
+        /// If two points have the same angle, they are compared based on their distance.
+        /// </summary>
+        class GrahamScanComp : IComparer<int>
+        {
+            private int orId;
+            private Vector[] points;
+
+            public GrahamScanComp(int orId, Vector[] points)
+            {
+                this.orId = orId;
+                this.points = points;
+            }
+
+            public int Compare(int x, int y)
+            {
+                // Returns:
+                //   < 0 if x is smaller
+                //   = 0 if x and y are equal
+                //   > 0 if x is larger
+
+                Vector origin = points[orId];
+                Vector vX = points[x];
+                Vector vY = points[y];
+
+                if (vX == vY) return 0;
+                if (vX == origin) return -1;
+                if (vY == origin) return 1;
+
+
+                // We define quadrants as follows:
+                //   1: [ -180, -90 )
+                //   2: [  -90,   0 )
+                //   3: [    0,  90 )
+                //   4: [   90, 180 )
+
+                int xQuad = -1;
+                int yQuad = -1;
+
+                if (vX.X < origin.X && vX.Y <= origin.Y) xQuad = 1;
+                else if (vX.X >= origin.X && vX.Y < origin.Y) xQuad = 2;
+                else if (vX.X > origin.X && vX.Y >= origin.Y) xQuad = 3;
+                else if (vX.X <= origin.X && vX.Y > origin.Y) xQuad = 4;
+
+                if (vY.X < origin.X && vY.Y <= origin.Y) yQuad = 1;
+                else if (vY.X >= origin.X && vY.Y < origin.Y) yQuad = 2;
+                else if (vY.X > origin.X && vY.Y >= origin.Y) yQuad = 3;
+                else if (vY.X <= origin.X && vY.Y > origin.Y) yQuad = 4;
+
+                if (xQuad != yQuad)
+                {
+                    return xQuad.CompareTo(yQuad);
+                }
+
+
+                // X and y are in same quadrant.
+                // Compare based on angle.
+
+                double scalar = GetScalar(origin, vX, vY);
+
+                if (scalar == 0.0)
+                {
+                    // All three points are on a straight line.
+                    // Compare based on distance.
+                    return (vX - origin).Length.CompareTo((vY - origin).Length);
+                }
+
+                return scalar.CompareTo(0.0);
+            }
         }
     }
 }
